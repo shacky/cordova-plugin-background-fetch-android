56d4fd084611fb0b79d9bc6db7cbacd48781e4a9
Sven Panko <sp@intuitiveminds.de>
2016-06-13 12:51:37 +0200

    bugfixes

--- src/android/WakeupService.java
+++ src/android/WakeupService.java
@@ -49,6 +49,9 @@ public class WakeupService extends IntentService {
             if (!released) {
                 Log.e(LOG_TAG, "timeout occurred while waiting for background task to finish");
             }
+            else {
+                Log.i(LOG_TAG, "received background finish signal");
+            }
 
             semaphores.remove(id);
         }
@@ -61,6 +64,8 @@ public class WakeupService extends IntentService {
         Semaphore semaphore = semaphores.get(id);
 
         if (semaphore != null) {
+            Log.i(LOG_TAG, "signalling finish to background task with id " + id);
+
             semaphore.release(1);
         } else {
             Log.e(LOG_TAG, "no running background task found while signalling execution finished for id " + id);
